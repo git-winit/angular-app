9201fba8503239c2dc486f3bb2a76ea29fd5d487
git-vinit <73235335+git-vinit@users.noreply.github.com>
2024-05-14 21:54:38 +0530

    Auto-generated commit

---
+++ server/chunk-BKTVTRGS.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as p,d as n,e as m,l as s,n as f,o as a,r as c,x as l}from"./chunk-SFLYS2AJ.mjs";var d=(()=>{let o=class o{constructor(){this.title="hello-world"}};o.\u0275fac=function(e){return new(e||o)},o.\u0275cmp=r({type:o,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(e,y){e&1&&(i(0,"p"),n(1,"Hello, Welcome to Vinits World!"),p())}});let t=o;return t})();var u=[];var v={providers:[l(u),a()]};var x={providers:[c()]},g=s(v,x);var h=()=>f(d,g),j=h;export{j as a};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-BKTVTRGS.mjs";import"./chunk-SFLYS2AJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
